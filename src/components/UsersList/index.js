4696a70b856288fe3d8ae504c4612dbc996fbf2f
Geomar Bastiani <geom@rbastiani.com>
2022-01-17 00:57:01 +0000

    fix bug setUsers

--- src/components/UsersList/index.js
+++ src/components/UsersList/index.js
@@ -2,27 +2,27 @@ import { useEffect, useState } from "react";
 import { api } from "../../helpers";
 import { User } from "..";
 
-const getUsers = async (setUsers, setIsLoading) => {
-  try {
-    const res = await api.get(`/api/users`);
-    setUsers(res.data);
-  } catch (err) {
-    console.log(err);
-  } finally {
-    setIsLoading(false);
-  }
-};
-
 const UsersList = ({ users, setUsers }) => {
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    getUsers(setUsers, setIsLoading);
+    const getUsers = async () => {
+      try {
+        const res = await api.get(`/api/users`);
+        setUsers(res.data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getUsers();
 
     return () => {
       api.cancel();
     };
-  }, []);
+  }, [setUsers]);
 
   if (isLoading) return <h1>Loading users...</h1>;
 
